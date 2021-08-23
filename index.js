dc85f548ec5b85205976053078466bc1ed6bb043
aniket7888 <aniketadmane922@gmail.com>
2021-08-23 08:09:00 +0530

    counter done

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 console.log("hello")
 
+let section =document.getElementById("section")
+
+let count = 0
+
+let val = document.createElement("h2")
+let incBtn =document.createElement("button")
+let decBtn =document.createElement("button")
+let reset =document.createElement("button")
+
+val.innerText = count
+incBtn.innerText ="Increment"
+decBtn.innerText ="Decrement"
+reset.innerText = "Reset"
+
+incBtn.onclick = function(){
+    count = count+1
+    val.innerText = count
+}
+
+decBtn.onclick = function(){
+    count -=1
+    val.innerText = count
+}
+ 
+reset.onclick = function(){
+    count = 0
+    val.innerText = count
+}
+section.append(val ,incBtn,decBtn,reset)
